b14101117dac37bdbd958ae734398e288dc7c2b7
Metabolisto <Metabolisto@gmail.com>
2025-11-26 20:12:28 +0400

    edited readme

--- Core/Src/system_stm32f10x.c
+++ Core/Src/system_stm32f10x.c
@@ -1,19 +1,19 @@
 #include "stm32f10x.h"
 
-uint32_t SystemCoreClock = 72000000; // Установи сюда свою рабочую частоту
+uint32_t SystemCoreClock = 72000000; // Operating frequency
 
 void SystemInit(void) {
-    RCC->CR |= RCC_CR_HSEON;                     // Включаем HSE (внешний кварц)
-    while (!(RCC->CR & RCC_CR_HSERDY));          // Ждём, пока HSE стабилизируется
+    RCC->CR |= RCC_CR_HSEON;                     // Turning on HSE (external quartz)
+    while (!(RCC->CR & RCC_CR_HSERDY));          // Waiting for the HSE to stabilize
 
-    RCC->CFGR |= RCC_CFGR_PLLSRC;                // PLL от HSE
-    RCC->CFGR |= RCC_CFGR_PLLMULL9;              // Множитель 9 -> 8 МГц * 9 = 72 МГц
+    RCC->CFGR |= RCC_CFGR_PLLSRC;                // PLL from HSE
+    RCC->CFGR |= RCC_CFGR_PLLMULL9;              // Multiplier 9 -> 8 MHz * 9 = 72 MHz
 
-    RCC->CR |= RCC_CR_PLLON;                     // Включаем PLL
-    while (!(RCC->CR & RCC_CR_PLLRDY));          // Ждём, пока PLL стабилизируется
+    RCC->CR |= RCC_CR_PLLON;                     // Enabling PLL
+    while (!(RCC->CR & RCC_CR_PLLRDY));          // Waiting for the PLL to stabilize
 
-    RCC->CFGR |= RCC_CFGR_SW_PLL;                // Переключаем системную тактовую на PLL
-    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL); // Ждём завершения переключения
+    RCC->CFGR |= RCC_CFGR_SW_PLL;                // Switching the system clock to PLL
+    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL); //Waiting for the switch to be completed
 }
 
 
